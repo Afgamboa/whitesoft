78fb9838e27837ee41e4be530e019c490b8ce152
Andresfg24 <andresfg2124@gmail.com>
2023-06-01 09:29:16 -0500

    inital commit front

---
+++ src/components/Home.jsx
@@ -0,0 +1,102 @@
+import react, { useEffect, useState } from "react";
+import "./css/Home.css";
+import { getCountries, newUser } from "./js/home.js";
+
+const Home = () => {
+  const [username, setUsername] = useState("");
+  const [userCountry, setUserCountry] = useState("");
+  const [countries, setCountries] = useState(null);
+  const [errors, setErrors] = useState("");
+  const [success, setSuccess] = useState("");
+
+  useEffect(() => {
+    const getCountriesAsync = async () => {
+      try {
+        const res = await getCountries();
+        const countries = res;
+        setCountries(countries);
+      } catch (e) {
+        console.log(e);
+        setErrors(e.message);
+      setTimeout(() => {
+        setErrors("");
+      }, 2500);
+      }
+    };
+    getCountriesAsync();
+  }, [countries]);
+
+  const handleSubmit = async () => {
+    try {
+      const res = await newUser(username, userCountry);
+      setSuccess(res.message);
+      setTimeout(() => {
+        setSuccess("");
+      }, 2500);
+      setUsername("");
+    } catch (e) {
+      setErrors(e.message);
+      setTimeout(() => {
+        setErrors("");
+      }, 2500);
+    }
+  };
+
+  return (
+    <div>
+      <div className="container-main card">
+        <div className="card-body sub-container col">
+          <h1 className="mb-5">WHITESOFT</h1>
+          <input
+            type="text"
+            className="form-control mb-3 username"
+            placeholder="Escriba su nombre aqui.."
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <select
+            name="countries"
+            aria-label=".form-select"
+            onChange={(e) => setUserCountry(e.target.value)}
+            className="form-select mb-3 countrie"
+            id="countries"
+          >
+            {countries && (
+              <>
+                <option value="" defaultValue>Pais...</option>
+                {countries.map((country, index) => (
+                  <option key={index} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </>
+            )}
+          </select>
+          <button
+            className="btn btn-outline-light"
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            enviar
+          </button>
+        </div>
+      </div>
+      <div
+        className="mt-2"
+        style={{
+          width: 400,
+          margin: "auto",
+          textAlign: "center",
+          fontWeight: 600,
+        }}
+      >
+        {success && <div className="alert alert-success">{success}</div>}
+        {errors && <div className="alert alert-danger">{errors}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/js/home.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001";
+
+export const getCountries = async () => {
+  const response = await axios.get(`${baseUrl}/countries`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if(response.status === 200) {
+    return response.data.countries;
+  }else{
+    throw new Error('Server Error')
+  }
+};
+
+export const newUser = async (username, country) => {
+  if(username.trim().length === 0 || country.trim().length === 0){
+    throw new Error("Ambos campos son requeridos!")
+  }
+
+  if (!/^([a-zA-Z]+\s)*[a-zA-Z]+$/.test(username)) {
+    throw new Error("Nombre de usuario incorrecto");
+  }
+  const response = await axios.post(`${baseUrl}/user/new`, {username, country}, {
+    headers: {
+      "Content-Type": "application/json",
+    }
+  });
+  return response.data;
+}
