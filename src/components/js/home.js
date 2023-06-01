6e3854a7853317b1f6d6b16cd84f9e08edf8f963
Andresfg24 <andresfg2124@gmail.com>
2023-06-01 16:31:19 -0500

    base url changed to prod

--- src/components/js/home.js
+++ src/components/js/home.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001";
+const baseUrl = "ec2-54-167-121-27.compute-1.amazonaws.com";
 
 export const getCountries = async () => {
   const response = await axios.get(`${baseUrl}/countries`, {
